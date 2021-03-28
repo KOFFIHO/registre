d8b6ad8122c861890272bf4ba1c112e5850c21ad
KOFFIHO <honorefare@gmail.com>
2021-03-28 03:09:23 +0200

    commit deux

--- docteur/settings.py
+++ docteur/settings.py
@@ -120,5 +120,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATICFILES_DIRS = [   BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = "/abc/"
-MEDIA_ROOT = BASE_DIR
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
